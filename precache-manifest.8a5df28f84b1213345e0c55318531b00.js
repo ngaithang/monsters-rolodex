863f238768ac0d389f260e37fd641fd939b80716
sean-choo <sean.choo@ada-asia.com>
2020-10-06 12:12:25 +0800

    Updates

---
+++ precache-manifest.8a5df28f84b1213345e0c55318531b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bd1cc6c1ef157148d95f23c7a9db3b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b98f1da46ea86fd56ad1",
+    "url": "/monsters-rolodex/static/css/main.17cd0a38.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98f1da46ea86fd56ad1",
+    "url": "/monsters-rolodex/static/js/main.54fe11a5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
